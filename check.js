93c9ffc018ba307a834760f47950f9b554669a61
StupidX2 <luckyzp77@gmail.com>
2025-03-17 21:18:23 +0700

    Update check.js

--- check.js
+++ check.js
@@ -1,10 +1,15 @@
 export default function handler(req, res) {
   const { ip, port, host = 'speed.cloudflare.com', tls = 'true' } = req.query;
+
+  if (!ip || !port) {
+    return res.status(400).json({ error: "IP dan Port wajib diisi!" });
+  }
+
   res.status(200).json({
     ip,
     port,
     host,
     tls,
-    message: 'Proxy check API is working!',
+    status: 'Proxy check API is working!',
   });
 }
